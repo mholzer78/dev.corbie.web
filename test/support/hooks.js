71e9c1d0285327b56d3d42e0624046933d2106d3
mholzer78 <michael@die-holzers.at>
2026-05-28 12:01:17 +0200

    adding tests

--- test/support/hooks.js
+++ test/support/hooks.js
@@ -19,7 +19,7 @@ Before(async function () {
   this.page = await this.context.newPage();
 });
 
-After(async function () {
+After(async function (scenario) {
   await this.page?.close();
   await this.context?.close();
 });
